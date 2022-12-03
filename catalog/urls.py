f5334006125a9df5fd78bad679469ca72944f5db
gr402_bomse <notmax2000@mail.ru>
2022-12-03 08:25:11 +0700

    chavo

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     url(r'^books/$', views.book_list, name='book_list'),
     url(r'^book/(?P<pk>\d+)/$', views.book_detail, name='book_detail'),
     url(r'^authors/$', views.author_list, name='author_list'),
-    url(r'^author/(?P<pk>\d+)/$', views.author_detail, name='author_detail'),
+    url(r'^author/(?P<pk>\d+)/$', views.author_detail, name='author_detail')
 ]
